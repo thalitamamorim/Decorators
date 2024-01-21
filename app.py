1b05ea26342149be2ca83ffd602790a1c4e07857
Thalita Amorim <156582822+thalitamamorim@users.noreply.github.com>
2024-01-21 20:47:05 -0300

    argumento

--- app.py
+++ app.py
@@ -1,19 +1,6 @@
-# Desafio 1
-nome = input('Qual seu nome completo? \n')
+# Desafio agurmentos posicionais e nomeados
+def gerar_objeto_personalizado(cor, *, altura, formato):
+    print (f'{cor}, {altura}, {formato}')
 
+gerar_objeto_personalizado('Preto', altura=1.5, formato='triangular')
 
-def gerar_nome_completo(nome=nome):
-    print(f'Bem vindo,{nome}!')
-
-
-gerar_nome_completo()
-
-# Desafio 2
-
-
-def calcular_valores(preco, quantidade=1):
-    total = preco * quantidade
-    print(f'O preço do produto é de: \n R$ {total}.')
-
-
-calcular_valores(23, 15)
